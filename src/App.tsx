20c2c7ad0f3d938c4ab581bf9336d64497776e07
James Duty <JDD89@pitt.edu>
2024-03-26 12:21:37 -0400

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useEffect } from 'react';
+import './App.css';
+import PokeCard from './PokeCard';
+
+function App() {
+
+  useEffect(() => {
+    
+  }, []);
+
+  return (
+    <div className="App">
+      <PokeCard />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/PokeCard.tsx
@@ -0,0 +1,14 @@
+import { useEffect } from 'react';
+import './PokeCard.css'
+
+export default function PokeCard(){
+
+    return(
+        <div className="card">
+            <div>
+                
+            </div>
+            <p>Swampert</p>
+        </div>
+    );
+}
